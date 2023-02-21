18029bf504b12a800422ecfb6900d77b5757c955
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-02-21 16:18:52 +0530

    Adding welcome message

---
+++ Day11And12ObjectOrientedPrograms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day11And12ObjectOrientedPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the object oriented programs");
+        }
+    }
+}
